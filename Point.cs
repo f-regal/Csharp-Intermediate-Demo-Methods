00e14fb6db2740f0b8253b5bddff4d197f2c4d1e
Faysal Regal <faysalregal@gmail.com>
2022-09-24 20:05:25 +0100

    created point class

---
+++ Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Intermediate_Demo_Methods
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            if (newLocation == null)
+                throw new ArgumentNullException("newLocation");
+            else
+                Move(newLocation.X, newLocation.Y);
+
+        }
+
+
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Csharp_Intermediate_Demo_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var point = new Point(10,20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0},{1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0},{1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured");
+                //CONTINUE FROM MIN 18.02
+            }
+        }
+    }
+}
